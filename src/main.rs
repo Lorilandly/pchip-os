84bdd79a2b8926b6fbaff9d232f19b1b44300704
Haoan Li <lihaoan1001@163.com>
2023-06-12 18:19:55 +0900

    Replace virt-uart with crate and add mutex
    Use code that someone else wrote reduce the chance for me to make a mistake. Add mutex so
    that one code is writing to uart at a time.
    
    Signed-off-by: Haoan Li <lihaoan1001@163.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
-mod asm;
+mod assembly;
 pub mod virt_uart;
 
 use core::panic::PanicInfo;
@@ -11,14 +11,8 @@ use riscv::asm::wfi;
 pub extern "C" fn main() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
-    let mut uart = virt_uart::Uart::new(0x1000_0000);
-    uart.init();
-    uart.put(b'h');
-    uart.put(b'e');
-    uart.put(b'l');
-    uart.put(b'l');
-    //println!("This is my operating system!");
-	//println!("I'm so awesome. If you start typing something, I'll show you what you typed!");
+    println!("This is my operating system!");
+	println!("I'm so awesome. If you start typing something, I'll show you what you typed!");
 
     
     loop{
@@ -28,6 +22,7 @@ pub extern "C" fn main() -> ! {
 
 #[no_mangle]
 pub extern "C" fn trap_handle() -> ! {
+    println!("trap");
     loop{
         unsafe { wfi() };
     }
@@ -35,7 +30,8 @@ pub extern "C" fn trap_handle() -> ! {
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop{
         unsafe { wfi() };
     }
--- src/virt_uart.rs
+++ src/virt_uart.rs
@@ -1,48 +1,39 @@
+use ns16550a::*;
 use core::fmt;
+use spin::Mutex;
+use lazy_static::lazy_static;
 
-pub struct Uart {
-    base_address: usize,
-}
-
-impl Uart {
-    pub fn new(base_address: usize) -> Self {
-        Self { base_address }
-    }
-    
-    pub fn init(&mut self) {
-		let ptr = self.base_address as *mut u8;
-		unsafe {
-            let lcr: u8 = (1 << 0) | (1 << 1);
-			ptr.add(3).write_volatile(lcr);
-            ptr.add(2).write_volatile(1 << 0);
-            ptr.add(1).write_volatile(1 << 0);
-            let divisor: u16 = 592;
-			let divisor_least: u8 =
-				(divisor & 0xff).try_into().unwrap();
-			let divisor_most: u8 =
-				(divisor >> 8).try_into().unwrap();
-            ptr.add(3).write_volatile(lcr | 1 << 7);
-            ptr.add(0).write_volatile(divisor_least);
-			ptr.add(1).write_volatile(divisor_most);
-            ptr.add(3).write_volatile(lcr);
-		}
-    }
-
-    pub fn put(&mut self, c: u8) {
-        let ptr = self.base_address as *mut u8;
-        unsafe {
-            ptr.add(0).write_volatile(c);
-        }
-    }
-}
-
-impl fmt::Write for Uart {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        for b in s.bytes() {
-            self.put(b);
-        }
-        Ok(())
-    }
+/*
+pub static SERIAL: Lazy<Mutex<Uart>> = Lazy::new(|| {
+    let uart = Uart::new(0x1000_0000);
+    uart.init(
+        WordLength::EIGHT,
+        StopBits::ONE,
+        ParityBit::DISABLE,
+        ParitySelect::EVEN,
+        StickParity::DISABLE,
+        Break::DISABLE,
+        DMAMode::MODE0,
+        Divisor::BAUD115200,
+    );
+    Mutex::new(uart)
+});
+*/
+lazy_static! {
+    pub static ref SERIAL: Mutex<Uart> = {
+        let uart = Uart::new(0x1000_0000);
+        uart.init(
+            WordLength::EIGHT,
+            StopBits::ONE,
+            ParityBit::DISABLE,
+            ParitySelect::EVEN,
+            StickParity::DISABLE,
+            Break::DISABLE,
+            DMAMode::MODE0,
+            Divisor::BAUD115200,
+        );
+        Mutex::new(uart)
+    };
 }
 
 #[macro_export]
@@ -65,5 +56,5 @@ macro_rules! println {
 #[doc(hidden)]
 pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
-    Uart::new(0x1000_0000).write_fmt(args).unwrap();
+    SERIAL.lock().write_fmt(args).unwrap();
 }
